1aa22aa57e3a22de6664892495830a839a3c1c25
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2012-03-19 18:17:25 +0000

    SubLayers processing added to Py script.

--- org.openworm.wormbrowser.utils/org/openworm/wormbrowser/utils/GroupingsAndPartInfoGeneration.py
+++ org.openworm.wormbrowser.utils/org/openworm/wormbrowser/utils/GroupingsAndPartInfoGeneration.py
@@ -28,8 +28,19 @@ def getIndented(dictionary,order):
     s=""
     for key in order:
         s+="\n"+key+"\n"
-        for value in dictionary[key]:
+        if type(dictionary[key]) is dict:
+            for value in dictionary[key]:
+                s+="\t"+value+"\n"
+                for subvalue in dictionary[key][value]:
+                    s+="\t\t"+subvalue+"\n"
+        else:
             s+="\t"+value+"\n"
+    for key in dictionary:
+        if key not in order:
+            #this is a sublayer
+            s+="\n"+key+"\n"
+            for value in dictionary[key]:
+                s+="\t"+value+"\n"
     return s
         
 
@@ -62,7 +73,7 @@ def generateMaterialsBasedGrouping(js_filename):
                         materials[m]=[]
     return materials
 
-def writeGroupingsToFile(js_filename,materials,order):
+def writeGroupingsToFile(js_filename,materials,order,sublayers):
     groupings="worm_body\n"
     for key in order:
         groupings+="\t"+key+"\n"
@@ -81,16 +92,17 @@ def customizeMaterialBasedGroupings(materials,customization):
     return groupings
 
 
-def generatePartsInfo(groupings, js_filename,order):
+def generatePartsInfo(groupings, js_filename,order,sublayers):
     parts_info = "# Parts\n"
     eid=5000
     for partlist in groupings.values():
-        for part in partlist:
-            parts_info+="\n"+part+"\n"
-            parts_info+="\t id: "+ str(eid) +"\n"
-            parts_info+="\t type: part\n"
-            parts_info+="\t display_name: "+part+"\n"
-            eid+=1
+        if type(partlist) is not dict:
+            for part in partlist:
+                parts_info+="\n"+part+"\n"
+                parts_info+="\t id: "+ str(eid) +"\n"
+                parts_info+="\t type: part\n"
+                parts_info+="\t display_name: "+part+"\n"
+                eid+=1
     parts_info += "\n"
     parts_info += "# Layers\n"
     eid=10
@@ -102,6 +114,20 @@ def generatePartsInfo(groupings, js_filename,order):
         parts_info+="\t layer: yes\n"
         parts_info+="\t hidden: yes\n"
         eid+=1
+    if sublayers is not None:
+        parts_info += "\n"
+        parts_info += "# Sub-Layers\n"
+        for layer in sublayers.keys():
+            subindex=0
+            for sublayer in sublayers[layer] :
+                    parts_info+="\n"+sublayer+"\n"
+                    parts_info+="\t id: "+ str(eid) +"\n"
+                    parts_info+="\t type: sublayer\n"
+                    parts_info+="\t display_name: "+sublayer+"\n"
+                    parts_info+="\t sublayer_index: "+str(subindex)+"\n"
+                    parts_info+="\t hidden: yes\n"
+                    subindex+=1
+                    eid+=1
     parts_info += "\n"
     parts_info += "# Group\n"
     parts_info +="\nworm_body\n"
@@ -111,42 +137,56 @@ def generatePartsInfo(groupings, js_filename,order):
     createFile(parts_info,PARTS_INFO_FILE)
     return
 
+def processGroupungsSubLayers(groupings,sublayers):
+    for layer in sublayers.keys():
+        groupings[layer]={}
+        for sublayer in sublayers[layer]:
+            groupings[layer][sublayer]=groupings[sublayer] 
+    return groupings
 
-def generateGroupings(js_filename, customization,order):
+def generateGroupings(js_filename, customization,order,sublayers):
     materials=generateMaterialsBasedGrouping(js_filename)
     if(customization is not None):
         materials=customizeMaterialBasedGroupings(materials, customization)
-    writeGroupingsToFile(js_filename,materials,order)
+    if(sublayers is not None):
+        materials=processGroupungsSubLayers(materials,sublayers)
+    writeGroupingsToFile(js_filename,materials,order,sublayers)
     return materials
 
-def generateGroupingsAndPartsInfo(js_filename, customization,order):
-    groupings = generateGroupings(js_filename, customization,order)
-    generatePartsInfo(groupings,js_filename,order)
+def generateGroupingsAndPartsInfo(js_filename, customization,order,sublayers):
+    groupings = generateGroupings(js_filename, customization,order,sublayers)
+    generatePartsInfo(groupings,js_filename,order,sublayers)
     
     
     
 ##########
 
-LAYERS_ORDER = ['HypCutLayer','MuscleLayer','SensNeurLayer','MotNeurLayer','IntNeurLayer','SocketLayer','SheathLayer','NUFsLayer','PolyNeurLayer','GLRLayer','ExcSysLayer','RectLayer','PharynxLayer','RepSysLayer','IntestineLayer']
+LAYERS_ORDER = ['Cuticle','Organs','Muscles','Neurons']
+
+SUBLAYERS = { 'Cuticle':['HypCutLayer'],
+              'Muscles':['MuscleLayer'],
+              'Neurons':['SensNeurLayer','MotNeurLayer','IntNeurLayer','PolyNeurLayer','SocketLayer','SheathLayer','NUFsLayer'],
+              'Organs':['ExcSysLayer','PharynxLayer','RepSysLayer','IntestineLayer','GLRLayer','RectLayer'],
+             }
 
 CUSTOMIZATION = {'HypCutLayer':['hypodermis_layer','arcade_cells_layer','seam_cell_layer'],
                  'MuscleLayer':['body_wall_muscle_layer','uterine_muscle_layer','vulval_muscle_layer', 'stomatoint_muscle_layer'],
-                 'SensNeurLayer':['sensoryneuron_layer'],
-                 'MotNeurLayer':['motor_neuron_layer'],
+                 'SensNeurLayer':['sensoryneuron_layer','head_mesodermal_cell_layer'],
+                 'MotNeurLayer':['motor_neuron_layer','excretory_cell_layer','glr_cells_layer'],
                  'IntNeurLayer':['interneuron_layer','temp_drg_color_layer'],
                  'SocketLayer':['socketcell_layer'],
                  'SheathLayer':['sheathother_layer','coelomocyte_layer'],
                  'NUFsLayer':['neurunkfunc_layer'],
                  'PolyNeurLayer':['polymodalneuron_layer'],
-                 'GLRLayer':['germline_layer','glr_cells_layer'],
-                 'ExcSysLayer':['excretory_gland_cells_layer','excretory_cell_layer','excretory_pore_cell_layer','excretory_duct_cell_layer'],
+                 'GLRLayer':['germline_layer'],
+                 'ExcSysLayer':['excretory_gland_cells_layer','excretory_pore_cell_layer','excretory_duct_cell_layer'],
                  'RectLayer':['rectal_epithelium_layer','sphnc_&_anal_dep_musc_layer','vpi_&_vir_layer'],
-                 'PharynxLayer':['phary_&_rect_glands_layer','pharyngeal_epithelium_layer','head_mesodermal_cell_layer','even_layer','marginal_cells_layer','odd_layer'],
+                 'PharynxLayer':['phary_&_rect_glands_layer','pharyngeal_epithelium_layer','even_layer','marginal_cells_layer','odd_layer'],
                  'RepSysLayer':['vulva_epithelium_layer','spermatheca_layer','uterus_layer','spermath_uterin_valve_layer','dtc_&_somatic_gonad_layer'],
                  'IntestineLayer':['intestine_layer']                
                  }
 
-generateGroupingsAndPartsInfo(DATA_FOLDER+JS_FILE,CUSTOMIZATION,LAYERS_ORDER)
+generateGroupingsAndPartsInfo(DATA_FOLDER+JS_FILE,CUSTOMIZATION,LAYERS_ORDER,SUBLAYERS)
 
 
 if __name__ == '__main__':
